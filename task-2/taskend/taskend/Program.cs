2550f75d7827da036b6f92e9aea88642b70c25f1
vasifalasgarzade <vasiffa@code.edu.az>
2019-08-25 15:37:41 -0700

    task add

---
+++ task-2/taskend/taskend/Program.cs
@@ -0,0 +1,95 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ad (Name)");
+            string Ad = Console.ReadLine();
+
+            Console.WriteLine("Soyad (Surname)");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("ID");
+            int idNumber = Convert.ToInt32(Console.ReadLine());
+
+            Usermellumatlari Hesabim = new Usermellumatlari(Ad, soyad, idNumber);
+
+            int selected = -1;
+
+            while (selected != 0)
+            {
+                Console.WriteLine();
+                selected = displayMenu();
+                switch (selected)
+                {
+                    case 1:
+                        Console.WriteLine("Hesabin balansi:" + Hesabim.Balance());
+                        break;
+                    case 2:
+                        Console.WriteLine("Mebleg daxil edin");
+                        int addHesab = Convert.ToInt32(Console.ReadLine());
+                        Hesabim.addToBalance(addHesab);
+                        break;
+                    case 3:
+                        Console.WriteLine("Mebleg daxil edin");
+                        int withdrawAmount = Convert.ToInt32(Console.ReadLine());
+                        Hesabim.withdrawFromBalance(withdrawAmount);
+                        break;
+                    case 4:
+                        Console.WriteLine("Ad (Name)");
+                        string updatedName = Console.ReadLine();
+                        if (String.IsNullOrEmpty(updatedName))
+                        {
+                            updatedName = Hesabim.FirstName();
+                        }
+
+                        Console.WriteLine("Soyad (Surname)");
+                        string updateSurname = Console.ReadLine();
+                        if (String.IsNullOrEmpty(updateSurname))
+                        {
+                            updatedName = Hesabim.LastName();
+                        }
+
+                        Console.WriteLine("ID number");
+                        string updatedIdNumberString = Console.ReadLine();
+                        int updatedIdNumber;
+                        if (String.IsNullOrEmpty(updatedIdNumberString))
+                        {
+                            updatedIdNumber = Hesabim.IdNumber();
+                        }
+                        else
+                        {
+                            updatedIdNumber = Convert.ToInt32(updatedIdNumberString);
+                        }
+
+                        Hesabim.updateCredentials(updatedName, updateSurname, updatedIdNumber);
+                        break;
+                    case 0:
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static int displayMenu()
+        {
+            Console.WriteLine("1. Balansa bax");
+            Console.WriteLine("2. Balans artir");
+            Console.WriteLine("3. Balansdan sil");
+            Console.WriteLine("4. Hesab məlumatlarını dəyiş");
+            Console.WriteLine("0. Cixish");
+
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ task-2/taskend/taskend/usermellumatlari.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Usermellumatlari
+    {
+        public string firstName;
+
+        public string lastName;
+
+        public int idNumber;
+
+        public int balance = 0;
+
+        public Usermellumatlari(string firstName, string lastName, int idNumber)
+        {
+            this.updateCredentials(firstName, lastName, idNumber);
+        }
+
+        public void updateCredentials(string firstName, string lastName, int idNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.idNumber = idNumber;
+        }
+
+        public string FirstName()
+        {
+            return this.firstName;
+        }
+
+        public string LastName()
+        {
+            return this.lastName;
+        }
+
+        public int IdNumber()
+        {
+            return this.idNumber;
+        }
+
+        public int Balance()
+        {
+            return this.balance;
+        }
+
+        public void withdrawFromBalance(int hesab)
+        {
+            if (this.balance >= hesab)
+            {
+                this.balance = this.balance - hesab;
+            }
+        }
+
+        public void addToBalance(int amount)
+        {
+            this.balance = this.balance + amount;
+        }
+    }
+}
